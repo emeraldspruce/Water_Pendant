25c732e81b850b547ef0bbad313ae41b2e89f2dd
emeraldspruce <emeraldspruce2005@gmail.com>
2025-03-24 03:53:18 -0400

    Began making the Grid

--- FLIP.h
+++ FLIP.h
@@ -4,13 +4,14 @@
 #define PARTICLE_NUM 75 // Water particles
 #define DIAMETER 15
 #define WATER_DENSITY 1000
-#define SIZE_OF_BIT_STATE 16
+#define SIZE_OF_BIT_STATE 6
 
 
 
 // Represents the return value of the simulator where the on/off state of each LED is stored.
-struct bitsState {
-    long bits[SIZE_OF_BIT_STATE]; // 10 * 32 = 320 bits max
+struct bitsState 
+    {
+    long bits[SIZE_OF_BIT_STATE]; // 6 * 32 = 192 bits max
 
     // Returns a 32 bit section of the state
     long operator[](unsigned long index)
@@ -30,7 +31,7 @@ struct bitsState {
         {
         bitsState newBits;
         for (unsigned long i = 0; i < SIZE_OF_BIT_STATE; i++) 
-            bits[i] = bits[i] & other[i];
+            newBits.bits[i] = bits[i] & other[i];
         return newBits;
         }
 
@@ -42,40 +43,80 @@ struct bitsState {
             bits[i] = bits[i] | other[i];
         return newBits;
         }
-};
+    };
 
 
 
 // Represents a particle in the simulation.
 struct Partical
     {
-    short position[2];
-    short velocity[2];
+    short position; // bits 0-7 are x, 7-15 are y
+    long velocity; // bits 0-15 are x, 16-31 are y
 
     Partical()
         {
-        position[0] = 0;
-        position[1] = 0;
-        velocity[0] = 0;
-        velocity[1] = 0;
+        position = 0; 
+        velocity = 0;
         }
-
-    Partical(long pos1, long pos2, long vel1, long vel2)
-        {
-        position[0] = pos1;
-        position[1] = pos2;
-        velocity[0] = vel1;
-        velocity[1] = vel2;
-        }
-    };
-
-
-
-// Representation of the Grid for storing velocities
-struct Grid
-    {
     
+    /**
+     * @param pos1 The x component of position
+     * @param pos2 The y component of position
+     * @param vel1 The x component of velocity
+     * @param vel2 The y component of velocity
+     */
+    Partical(char pos1, char pos2, short vel1, short vel2)
+        {
+        position = pos1 | (pos2 << 8);
+        velocity = vel1 | (vel2 << 16);
+        }
     };
+
+
+
+// Represents a single cell in the simulation grid.
+struct GridCell {
+    long velocity; // Velocity components (x, y)
+    short weight;  // Weight for velocity normalization
+
+    GridCell() : velocity(0), weight(0) {}
+
+    inline void velPack(short x, short y) 
+        {
+        this->velocity = static_cast<long>(x) | (static_cast<long>(y) << 16);
+        }
+
+    void operator=(GridCell other)
+        {
+        this->weight = other.weight;
+        velPack((short)other.velocity, other.velocity >> 8);
+        }
+};
+
+
+
+// Represents the simulation grid.
+struct Grid {
+    GridCell cells[DIAMETER][DIAMETER];
+
+    void clear()
+        {
+        for (unsigned short i = 0; i < DIAMETER; ++i)
+            for (unsigned short j = 0; j < DIAMETER; ++j)
+                {
+                cells[i][j].velPack(0, 0);
+                cells[i][j].weight = 0;
+                }
+        }
+
+    void operator=(Grid other)
+        {
+        for (unsigned short i = 0; i < DIAMETER; ++i)
+            for (unsigned short j = 0; j < DIAMETER; ++j)
+                this->cells[i][j] = other.cells[i][j];
+        }
+};
+
 
 
 // Performs the FLIP fluid simulation
